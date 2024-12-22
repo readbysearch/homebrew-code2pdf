1dba384c46cac486030985c8db76af6fd95a5eae
readbysearch <readbysearch@users.noreply.github.com>
2024-12-22 13:58:37 +0800

    Update to v1.0.1

--- code2pdf.rb
+++ code2pdf.rb
@@ -1,7 +1,7 @@
 class Code2pdf < Formula
   desc "Convert source code files to PDF with syntax highlighting"
   homepage "https://github.com/readbysearch/code2pdf"
-  url "https://github.com/readbysearch/code2pdf/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/readbysearch/code2pdf/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "96bb50355e35354c10f8f0563ccf8a372b32e21eae166f0a473603329d393eea"
   license "AGPL-3.0"
 
